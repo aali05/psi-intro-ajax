a291064bf1a0c21d75c433b1d35a3a60d199dd4e
Amal Ali <amal.musse.ali@gmail.com>
2017-04-18 22:21:37 -0500

    added style and appended movie info onto DOM

--- script.js
+++ script.js
@@ -10,13 +10,22 @@ function onReady() {
   // when response comes back from the server
   // run success function which will log response
   $.ajax({
-    url: 'http://www.omdbapi.com/?t=jaws',
+    url: 'http://www.omdbapi.com/?t=The+Godfather%3A+Part+II',
     success: function(response) {
       console.log('this is the response ->', response);
       console.log('movie title ->', response.Title);
       // console.log('movie actors ->', response.Actors);
       // put title on the DOM
+      $('#movie-holder').append( '<img src="'+ response.Poster +'">')
       $('#movie-holder').append('<div class="movie-title">' + response.Title + '</div>');
+      $('#movie-holder').append('<div class="movie-actors">' + response.Actors + '</div>');
+      $('#movie-holder').append('<div class="movie-release">' + response.Released + '</div');
+      $('#movie-holder').append('<div class="movie-plot">' + response.Plot + '</div>');
+      $('#movie-holder').append('<div class="movie-awards">' + response.Awards + '</div>');
+      // $('#movie-holder').prepend('<img id="theImg" src="Godfather.png" />');
+      // $('#movie-holder').append('<img src="' + response.Poster + '>');
+
+      // code wouldn't work for me.
     }
     // error: function(response) {
     //   console.log('error case: response ->', response);
