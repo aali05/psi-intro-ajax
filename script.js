0576b31e31ce69d15aa138a0057c730248cadfe3
Amal Ali <amal.musse.ali@gmail.com>
2017-04-18 22:28:18 -0500

    added comments to code

--- script.js
+++ script.js
@@ -14,21 +14,18 @@ function onReady() {
     success: function(response) {
       console.log('this is the response ->', response);
       console.log('movie title ->', response.Title);
-      // console.log('movie actors ->', response.Actors);
-      // put title on the DOM
+      // put movie poster on the DOM
       $('#movie-holder').append( '<img src="'+ response.Poster +'">')
+      // put title of the movie on the DOM
       $('#movie-holder').append('<div class="movie-title">' + response.Title + '</div>');
+      // put the actors on the DOM
       $('#movie-holder').append('<div class="movie-actors">' + response.Actors + '</div>');
+      // put date the movie was released on the DOM
       $('#movie-holder').append('<div class="movie-release">' + response.Released + '</div');
+      // put plot of the movie on the DOM
       $('#movie-holder').append('<div class="movie-plot">' + response.Plot + '</div>');
+      // put awards movie has won on the DOM
       $('#movie-holder').append('<div class="movie-awards">' + response.Awards + '</div>');
-      // $('#movie-holder').prepend('<img id="theImg" src="Godfather.png" />');
-      // $('#movie-holder').append('<img src="' + response.Poster + '>');
-
-      // code wouldn't work for me.
-    }
-    // error: function(response) {
-    //   console.log('error case: response ->', response);
-    // }
+  }
   });
 }
